707f767551f170ae06bc4b228e6f074cc8a302a3
MohamedAMoneem <mohamed.ahmed.rr2009@gmail.com>
2026-06-16 06:06:02 +0300

    modified project13.py added project14.py

---
+++ functionstraining/project2.py
@@ -0,0 +1,4 @@
+def multiply(num):
+    for i in range(10):
+        print(num, " X " , i ," = ", num*i)
+multiply(6)
---
+++ project15.py
@@ -0,0 +1,29 @@
+#this project decrypts whatever you write
+import string
+chars = string.ascii_lowercase
+chars_upper = string.ascii_uppercase
+other = string.punctuation
+numbers = string.digits
+word = input("insert something you want to decrypt: ")
+number = int(input("insert encryption number: "))
+def encrypt(sent,turns):
+    final = ""
+    for i in sent:
+        if i in other or i == " " or i in numbers:
+            final += i
+            continue
+        if i in chars:
+            pos = chars.index(i)
+            new_pos = pos - turns
+        else:
+            pos = chars_upper.index(i)
+            new_pos = pos - turns
+        if new_pos < 0:
+            new_pos += 26
+        if i in chars:
+            final += chars[new_pos]
+        else:
+            final += chars_upper[new_pos]
+
+    print(final)
+encrypt(word,number)
